e869f02d362228764c7aa4cd918e7ae5cb84d9f4
Andrew Pashinnik <tobirawork@gmail.com>
2018-03-11 02:04:45 +0200

    feat(src): add createRegexp, replaceKey and filter functions

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const createRegexp = key => new RegExp(String(key), 'gi')
+const replaceKey = filters => (text, key) =>
+	text.replace(createRegexp(key), filters[key])
+
+const filter = (text = '', filters = {}) =>
+	Object.keys(filters).reduce(replaceKey(filters), String(text))
+
+module.exports = filter
